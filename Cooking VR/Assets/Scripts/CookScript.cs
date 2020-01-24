f9e9a4815dbd8497caaf37370b2908f4dda04df7
CalvinOh <calvinohwyekit@gmail.com>
2020-01-23 19:01:32 -0600

    Add PattyCooking beginning
    Add Beginning of Cooking a Patty. Add functionality to keep track of how long the patty has been cooking for as well as the degree of doneness for the patty
    
    Co-Authored-By: Pedro Alonso Tresgallo <p3dro115@gmail.com>

---
+++ Cooking VR/Assets/Scripts/CookScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Doneness
+{
+    Raw,
+    Rare,
+    Medium,
+    WellDone,
+    Burnt
+};
+
+public class CookScript : MonoBehaviour
+{
+    public float currentCookAmount; // The progress of how much the item is cooked
+    public const float maxCookAmount = 60; // The max value for currentCookAmount
+    public Doneness doneness;
+
+    private float[] donenessRefs = { 10, 20, 30, 40 };
+
+    private bool currentlyCooking; // Is the object inside a cookBox? If true, will continue cooking.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCookAmount = 0;
+        doneness = Doneness.Raw;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentlyCooking && currentCookAmount <= maxCookAmount)
+        {
+            currentCookAmount += Time.deltaTime;
+
+            for(int i = 0; i < donenessRefs.Length; i++)
+            {
+                if(currentCookAmount > donenessRefs[i])
+                {
+                    switch(i)
+                    {
+                        case 0: // Rare
+                            {
+                                doneness = Doneness.Rare;
+                                break;
+                            }
+                        case 1: // Medium
+                            {
+                                doneness = Doneness.Medium;
+                                break;
+                            }
+                        case 2: // Well Done
+                            {
+                                doneness = Doneness.WellDone;
+                                break;
+                            }
+                        case 3: // Burnt
+                            {
+                                doneness = Doneness.Burnt;
+                                break;
+                            }
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(other.CompareTag("CookBox"))
+        {
+            currentlyCooking = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if(other.CompareTag("CookBox"))
+        {
+            currentlyCooking = false;
+        }
+    }
+}
