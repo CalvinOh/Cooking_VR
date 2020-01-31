01f6fc8742aaa7143e36907b40d5b01284f3d3d0
Jiening <Jiening.tao@loop.colum.edu>
2020-01-31 15:55:15 -0600

    ordersystem started with 3 scripts, commiting and pulling for new assets

---
+++ Cooking VR/Assets/OrderManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderManager : MonoBehaviour
+{
+
+    public class Order
+    {
+        //An order
+        public float TimeIssued;
+        public List<string> Ingredents;
+    }
+
+    public class FinishedOrder
+    {
+        //Completed order that is saved
+        public int TotalAmountOfIngredients;
+        public float Score;
+        public float TimeTaken;
+        public string Notes;
+
+    }
+
+    private List<FinishedOrder> finishedOrders = new List<FinishedOrder>();
+    public static List<Order> Orders = new List<Order>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
