d192f28c78b7fd974d011f99273e97ec16e4b6d8
Pedro A. Tresgallo <P3dro115@gmail.com>
2020-02-05 10:12:19 -0600

    Update Stacking
    (Still prototype)

--- Cooking VR/Assets/Scripts/Stacking.cs
+++ Cooking VR/Assets/Scripts/Stacking.cs
@@ -38,19 +38,46 @@ public class Stacking : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.GetComponent<Stacking>().stackable == true)
+        this.gameObject.GetComponent<Rigidbody>().velocity = collision.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        // If the other object is higher than the current object
+        if(collision.gameObject.GetComponent<Stacking>().stackable == true &&
+            collision.gameObject.transform.position.y >= this.gameObject.transform.position.y)
         {
-            currentParent = collision.gameObject;
-            this.gameObject.transform.parent = collision.transform;
-            this.transform.position = collision.transform.position;
-            this.transform.rotation = collision.transform.rotation;
-
-            isStacking = true;
+            if(collision.gameObject.transform.parent.gameObject != this.gameObject)
+            {
+                collision.gameObject.transform.parent = this.gameObject.transform;
+                collision.gameObject.transform.rotation = new Quaternion(0, collision.gameObject.transform.rotation.y, 0, collision.gameObject.transform.rotation.w);
+                isStacking = true;
+            }
         }
+        else if(this.gameObject.GetComponent<Stacking>().stackable == true &&
+            collision.gameObject.transform.position.y < this.gameObject.transform.position.y)
+            {
+                if(this.gameObject.transform.parent != collision.gameObject.transform)
+                {
+                    this.gameObject.transform.transform.parent = collision.gameObject.transform;
+                    this.gameObject.transform.rotation = new Quaternion(0, this.gameObject.transform.rotation.y, 0, this.gameObject.transform.rotation.w);
+                    isStacking = true;
+                }
+            }
+
+        //if(collision.gameObject.GetComponent<Stacking>().stackable == true)
+        //{
+        //    currentParent = collision.gameObject;
+        //    this.gameObject.transform.parent = collision.transform;
+        //    this.transform.position = collision.transform.position;
+        //    this.transform.rotation = collision.transform.rotation;
+
+        //    isStacking = true;
+        //}
     }
     private void OnCollisionExit(Collision collision)
     {
         isStacking = false;
+        if(false)
+        {
+
+        }
 
     }
 
