a947c9b765766ee1dc7840623449bbde522b2bd9
Pedro A. Tresgallo <P3dro115@gmail.com>
2020-02-05 10:02:30 -0600

    Add Mesh Alteration scripts

---
+++ Cooking VR/Assets/Scripts/ProceduralMesh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralMesh : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeMeshData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeMeshData()
+    {
+        vertices = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(1, 0, 1)};
+
+        triangles = new int[] { 0, 1, 2 };
+
+    }
+}
