ee405c2128dace33b1434d97f1de803e0f4a670d
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2020-02-24 15:16:25 -0700

    Menu Function Progress

---
+++ Cooking VR/Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    [SerializeField]
+    private int sceneNum = 1;
+
+    [SerializeField]
+    private int characterSize;
+
+    public void GoToScene(int sceneNum)
+    {
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+        SceneManager.LoadScene(sceneNum);
+    }
+
+    public void ResetPosition()
+    {
+        SettingsManager settingsManager = (SettingsManager)FindObjectOfType(typeof(SettingsManager));
+        settingsManager.ResetPosition();
+    }
+
+    public void SaveSettings()
+    {
+        SettingsManager settingsManager = (SettingsManager)FindObjectOfType(typeof(SettingsManager));
+        settingsManager.characterSize = characterSize;
+        settingsManager.ApplySettings();
+    }
+}
