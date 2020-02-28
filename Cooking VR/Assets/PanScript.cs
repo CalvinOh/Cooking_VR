dd444bc47b3b5d83f2adfea9432582b42ae8c037
Jiening <Jiening.tao@loop.colum.edu>
2020-02-28 16:22:16 -0600

    stove system

---
+++ Cooking VR/Assets/PanScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanScript : MonoBehaviour
+{
+    [SerializeField]
+    private bool IsHeated;
+
+    private PattyScript CurrentPattyOnPan;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Patty"))
+        {
+            CurrentPattyOnPan = other.GetComponent<PattyScript>();
+            if(IsHeated)
+            CurrentPattyOnPan.StartCooking();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Patty"))
+        {
+            CurrentPattyOnPan.StopCooking();
+            CurrentPattyOnPan = null;
+        }
+    }
+
+    public void StartCooking()
+    {
+        IsHeated = true;
+        CurrentPattyOnPan.StartCooking();
+    }
+
+    public void StopCooking()
+    {
+        IsHeated = false;
+        CurrentPattyOnPan.StopCooking();
+    }
+
+}
---
+++ Cooking VR/Assets/StoveScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveScript : MonoBehaviour
+{
+    [SerializeField]
+    private bool StoveIsOn;
+
+    private PanScript CurrentPanOnStove;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pan") )
+        {
+            CurrentPanOnStove = other.GetComponent<PanScript>();
+            if(StoveIsOn)
+            CurrentPanOnStove.StartCooking();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Pan"))
+        {
+            
+            CurrentPanOnStove.StopCooking();
+            CurrentPanOnStove = null;
+        }
+    }
+
+    public void TurnOn()
+    {
+        StoveIsOn = false;
+        CurrentPanOnStove.StartCooking();
+    }
+
+    public void TurnOff()
+    {
+        StoveIsOn = false;
+        CurrentPanOnStove.StopCooking();
+    }
+}
