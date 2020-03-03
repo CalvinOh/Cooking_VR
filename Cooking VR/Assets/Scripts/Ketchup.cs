9a7ebb81aa9b6ec9712925e02ada5113d184597c
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2020-03-03 12:41:16 -0600

    Updated Ketchup
    -Added Comments
    -Ketchup splat layers and rotates correctly against surfaces it hits.

--- Cooking VR/Assets/Scripts/Ketchup.cs
+++ Cooking VR/Assets/Scripts/Ketchup.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 
 public class Ketchup : MonoBehaviour
 {
-
+    //Rigidbody of the ketchup object
     Rigidbody rb;
 
-    Collider collider;
-
+    //Particle system that creates the midair spray
     ParticleSystem particle;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Grabs the ketchup's components
         rb = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
         particle = GetComponent<ParticleSystem>();
-        rb.AddForce(Vector3.forward * 300);
-        rb.AddForce(Vector3.up * 50);
+
+        //Test force on ketchup
+        //rb.AddForce(Vector3.forward * 300);
+        //rb.AddForce(Vector3.up * 50);
+        //rb.AddForce(Vector3.right * 50);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        //When the ketchup hits an object it deactivates the particle system, activates the splat and adjusts the splat to stick to the location it hits.
         particle.Stop();
         this.transform.GetChild(0).gameObject.SetActive(true);
         rb.isKinematic = true;
+        this.transform.SetPositionAndRotation(this.transform.position, collision.transform.rotation);
     }
 }
