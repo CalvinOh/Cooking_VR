6f23b45432396a224912f3c0987f09b99ea118a0
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2020-03-03 11:53:45 -0600

    Ketchup Spray Added
    -Ketchup Particle System added for when the ketchup is in the air
    -Ketchup sticks to wall
    -Ketchup Splat switches with particle system

---
+++ Cooking VR/Assets/Scripts/Ketchup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ketchup : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    Collider collider;
+
+    ParticleSystem particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        particle = GetComponent<ParticleSystem>();
+        rb.AddForce(Vector3.forward * 300);
+        rb.AddForce(Vector3.up * 50);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        particle.Stop();
+        this.transform.GetChild(0).gameObject.SetActive(true);
+        rb.isKinematic = true;
+    }
+}
