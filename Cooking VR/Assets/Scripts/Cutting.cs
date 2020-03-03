bbec3ae0cd84463d444db2892b20b9e3715968aa
CalvinOh <calvinohwyekit@gmail.com>
2020-03-03 14:57:02 -0600

    Tomato, Pickle & Cheese cutable prefabs for milestone 1

--- Cooking VR/Assets/Scripts/Cutting.cs
+++ Cooking VR/Assets/Scripts/Cutting.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class Cutting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnCollisionEnter(Collision collision)
     {
+
         if(collision.gameObject.CompareTag("Cutable"))
         {
             Debug.Log("Cutting");
