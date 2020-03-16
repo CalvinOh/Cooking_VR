a68f6e863f356e6c9cdda6bc27067f0a77ebf5c6
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2020-03-16 10:15:11 -0500

    Ketchup Changes
    -Ketchup should delete after 5 seconds if they don't hit or collide with an object that isn't stackable
    -Ketchup rotation upon collision should better represent the rotation of the surface it hit
    
    These changes need to be double checked in the VR space.

--- Cooking VR/Assets/Scripts/Ketchup.cs
+++ Cooking VR/Assets/Scripts/Ketchup.cs
@@ -10,6 +10,14 @@ public class Ketchup : MonoBehaviour
     //Particle system that creates the midair spray
     ParticleSystem particle;
 
+    bool hitActive = false;
+
+    bool hitNotStackable = false;
+
+    float timeStored;
+
+    float timeToDelete = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,12 +31,34 @@ public class Ketchup : MonoBehaviour
         //rb.AddForce(Vector3.right * 50);
     }
 
+    private void Update()
+    {
+        if (hitNotStackable)
+        {
+            timeStored = Time.time;
+        }
+
+        if(Time.time >= timeStored + 5 && hitNotStackable)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        //When the ketchup hits an object it deactivates the particle system, activates the splat and adjusts the splat to stick to the location it hits.
-        particle.Stop();
-        this.transform.GetChild(0).gameObject.SetActive(true);
-        rb.isKinematic = true;
-        this.transform.SetPositionAndRotation(this.transform.position, collision.transform.rotation);
+        if (!hitActive)
+        {
+            //When the ketchup hits an object it deactivates the particle system, activates the splat and adjusts the splat to stick to the location it hits.
+            particle.Stop();
+            this.transform.GetChild(0).gameObject.SetActive(true);
+            rb.isKinematic = true;
+            this.transform.SetPositionAndRotation(this.transform.position, rb.rotation);
+            rb.freezeRotation = true;
+            if (!collision.gameObject.GetComponent<Stackable>())
+            {
+                hitNotStackable = true;
+            }
+            hitActive = true;
+        }
     }
 }
