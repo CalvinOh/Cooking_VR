ffc1e7cdbc84e3c791aa2f504bb187fa0e38c926
Jiening <Jiening.tao@loop.colum.edu>
2020-04-17 12:52:09 -0500

    Added canned items gag

---
+++ Cooking VR/Assets/Canned.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canned : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private float TippingAngle;
+    [SerializeField]
+    private GameObject Contents;
+    [SerializeField]
+    private Transform SpawnPoint;
+    [SerializeField]
+    private int Amount =1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Current Angle: " + Vector3.Angle(transform.up, -Vector3.up) + " Tipped: " + Tipped());
+
+
+        if (Tipped()&&Amount>0)
+        {
+            Instantiate(Contents, SpawnPoint.transform.position, SpawnPoint.transform.rotation, null);
+            Amount--;
+        }
+    }
+
+
+
+    private bool Tipped()
+    {
+        return (Vector3.Angle(transform.up, -Vector3.up) < TippingAngle);
+    }
+}
