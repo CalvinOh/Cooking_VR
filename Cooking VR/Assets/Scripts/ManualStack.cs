91224c1ced86c7b28941bf5eafc3c51fc0107e5a
TheQuestCrafter <thequestcrafter@gmail.com>
2020-04-24 12:53:43 -0500

    Fixes for Beta Build

--- Cooking VR/Assets/Scripts/ManualStack.cs
+++ Cooking VR/Assets/Scripts/ManualStack.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UIElements;
 using Valve.VR.InteractionSystem;
