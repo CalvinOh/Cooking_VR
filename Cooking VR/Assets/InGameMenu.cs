f865b778a2a04c9ef346a0f0994c7b9e07045db7
Zoey Reyes <42983147+TheQuestCrafter@users.noreply.github.com>
2020-05-10 21:15:06 -0500

    In Level Menu Test

---
+++ Cooking VR/Assets/InGameMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour
+{
+    public void BackToMainMenu()
+    {
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(0));
+    }
+}
