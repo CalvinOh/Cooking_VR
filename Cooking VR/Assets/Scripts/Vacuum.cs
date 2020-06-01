fedf1c8c4b80c173962228d699f6df7ab3fde179
Pedro Alonso Tresgallo <P3dro115@gmail.com>
2020-06-01 15:22:00 -0500

    Add Vacuum and Grabbable G.O.
    Add Grabbable Game Object script that inherits from monobehaviour
    
    Add Vacuum script for vacuum prop
    
    Co-Authored-By: keeganjennings <keeganjennings@users.noreply.github.com>

---
+++ Cooking VR/Assets/Scripts/Vacuum.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vacuum : GrabbableGameObject
+{
+    [SerializeField]
+    Collider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitGrabbable();
+        if(col == null)
+        {
+            col = gameObject.GetComponent<BoxCollider>();
+            col.isTrigger = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGrabbed();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!other.tag.Equals("Plate") && other.gameObject.TryGetComponent<ManualStack>(out ManualStack ms))
+        {
+            Debug.Log($"{other.gameObject.name} was deleted by vacuum");
+            Destroy(other.gameObject);
+        }
+        else if(other.gameObject.TryGetComponent<Vacuum>(out Vacuum v))
+        {
+            Debug.Log($"You fool, you've created a paradox!");
+            Debug.Break();
+        }
+    }
+}
