3c90cd51cbba700150b51eb41919072da7372420
Jiening <Jiening.tao@loop.colum.edu>
2020-06-26 19:01:28 -0500

    started work on fuse and fuse box

---
+++ Cooking VR/Assets/Fuse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fuse : MonoBehaviour
+{
+    [SerializeField]
+    bool Broken;
+
+    private List<GameObject> SlotsInRange;
+
+
+    private void Start()
+    {
+        SlotsInRange = new List<GameObject>();
+    }
+
+    public void Break(bool broken)
+    {
+        Broken = broken;
+    }
+
+
+
+
+
+
+}
---
+++ Cooking VR/Assets/FuseBox.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseBox : MonoBehaviour
+{
+
+    private List<Fuse> AttachedFuses;
+
+    [SerializeField]
+    private GameObject Switch;
+
+
+
+
+    public void TriggerEvent(int BrokenFuseNum)
+    {
+        if (BrokenFuseNum == 0)
+        {
+        }
+        if (BrokenFuseNum == 1)
+        {
+            AttachedFuses[Random.Range(0, 2)].Break(true);
+        }
+        else if (BrokenFuseNum == 2)
+        {
+            foreach (Fuse a in AttachedFuses)
+            {
+                a.Break(true);
+            }
+            AttachedFuses[Random.Range(0, 2)].Break(false);
+        }
+        else
+        {
+            foreach (Fuse a in AttachedFuses)
+            {
+                a.Break(true);
+            }
+        }
+
+        //move switch to off position and prepare other things for evennt start
+
+
+
+    }
+
+    public void AttemptFix()
+    {
+        Switch.transform.parent = this.gameObject.transform;
+    }
+
+}
