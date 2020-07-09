be8c935e600e977b74c2c09040f8921c3b3676c6
kruegerc96 <49258382+kruegerc96@users.noreply.github.com>
2020-07-08 22:17:03 -0500

    began work on selective sounds script

---
+++ Cooking VR/Assets/Scripts/Audio/SelectiveSounds.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectiveSounds : MonoBehaviour
+{
+    // audio
+    // this script instructs the object to make collision sounds on collisions with certain objects
+    // but not with others, such as cuttable items.
+
+    public string WwiseEventName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckPrefabName();
+    }
+
+    void CheckPrefabName()
+    {
+        if (this.gameObject.name == "spatula_prefab")
+        {
+            WwiseEventName = "Impact_Spatula";
+        }
+
+        if (this.gameObject.name == "kitchen_knife")
+        {
+            WwiseEventName = "Impact_Knife";
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        AkSoundEngine.PostEvent(WwiseEventName, gameObject);
+    }
+}
