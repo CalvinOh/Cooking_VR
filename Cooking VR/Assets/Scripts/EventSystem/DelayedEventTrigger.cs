14ef95f0b981895f5610248548f703c854e97bca
Jiening <Jiening.tao@loop.colum.edu>
2020-09-15 15:06:41 -0500

    made more levels and adjusted some scripts to work with the level flow, added gianna eye tracking

---
+++ Cooking VR/Assets/Scripts/EventSystem/DelayedEventTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RandomEvent))]
+public class DelayedEventTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private float Delay;
+
+    private RandomEvent MyEvent;
+    void Start()
+    {
+        StartCoroutine(Delayedtrigger(Delay));
+        MyEvent = GetComponent<RandomEvent>();
+    }
+
+    private IEnumerator Delayedtrigger(float Delay)
+    {
+        yield return new WaitForSeconds(Delay);
+        MyEvent.TriggerNow();
+    }
+}
--- Cooking VR/Assets/Scripts/OrderSystem/Levels/Level6.cs
+++ Cooking VR/Assets/Scripts/OrderSystem/Levels/Level6.cs
@@ -21,5 +21,24 @@ public class Level6 : ILevels
 
         base.AddOrder(2, TempBurger, TempSides);
 
+        //burger 2
+        TempBurger = new List<OrderManager.Ingridents>();
+
+        TempBurger.Add(OrderManager.Ingridents.TopBun);
+        TempBurger.Add(OrderManager.Ingridents.Cheese);
+        TempBurger.Add(OrderManager.Ingridents.Pickle);
+        TempBurger.Add(OrderManager.Ingridents.WellDonePatty);
+        TempBurger.Add(OrderManager.Ingridents.Cheese);
+        TempBurger.Add(OrderManager.Ingridents.BottomBun);
+
+        base.AddOrder(10, TempBurger, TempSides);
+
+        //burger 3-7 (using random)
+        base.AddOrder(35, MyOrderSpawn.RandomBurger(3), MyOrderSpawn.RandomSides(0));
+        base.AddOrder(20, MyOrderSpawn.RandomBurger(4), MyOrderSpawn.RandomSides(1));
+        base.AddOrder(40, MyOrderSpawn.RandomBurger(4), MyOrderSpawn.RandomSides(0));
+        base.AddOrder(20,MyOrderSpawn.RandomBurger(2),MyOrderSpawn.RandomSides(1));
+        base.AddOrder(0, MyOrderSpawn.RandomBurger(2), MyOrderSpawn.RandomSides(1));
+
     }
 }
