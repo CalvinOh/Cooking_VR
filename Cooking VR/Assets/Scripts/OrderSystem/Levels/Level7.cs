428c7a64b3c8474138b089eed83c2e35dfc3b383
Jiening <Jiening.tao@loop.colum.edu>
2020-09-15 15:11:34 -0500

    finished level 7 script

--- Cooking VR/Assets/Scripts/OrderSystem/Levels/Level7.cs
+++ Cooking VR/Assets/Scripts/OrderSystem/Levels/Level7.cs
@@ -8,6 +8,32 @@ public class Level7 : ILevels
     // Start is called before the first frame update
     protected override void AddPredeterminedOrders()
     {
-        
+        List<OrderManager.Ingridents> TempBurger = new List<OrderManager.Ingridents>();
+        List<OrderManager.Sides> TempSides = new List<OrderManager.Sides>();
+
+        //burger 1-5 (using random)
+        base.AddOrder(35, MyOrderSpawn.RandomBurger(3), MyOrderSpawn.RandomSides(0));
+        base.AddOrder(20, MyOrderSpawn.RandomBurger(2), MyOrderSpawn.RandomSides(1));
+        base.AddOrder(0, MyOrderSpawn.RandomBurger(2), MyOrderSpawn.RandomSides(1));
+        base.AddOrder(20, MyOrderSpawn.RandomBurger(4), MyOrderSpawn.RandomSides(1));
+        base.AddOrder(40, MyOrderSpawn.RandomBurger(4), MyOrderSpawn.RandomSides(0));
+
+        //burger 6
+        TempBurger = new List<OrderManager.Ingridents>();
+
+        TempBurger.Add(OrderManager.Ingridents.TopBun);
+        TempBurger.Add(OrderManager.Ingridents.Cheese);
+        TempBurger.Add(OrderManager.Ingridents.RarePatty);
+        TempBurger.Add(OrderManager.Ingridents.WellDonePatty);
+        TempBurger.Add(OrderManager.Ingridents.Cheese);
+        TempBurger.Add(OrderManager.Ingridents.BottomBun);
+
+        base.AddOrder(10, TempBurger, TempSides);
+
+        //burger 7-9 (using random)
+        base.AddOrder(35, MyOrderSpawn.RandomBurger(3), MyOrderSpawn.RandomSides(0));
+        base.AddOrder(20, MyOrderSpawn.RandomBurger(2), MyOrderSpawn.RandomSides(1));
+        base.AddOrder(10, MyOrderSpawn.RandomBurger(4), MyOrderSpawn.RandomSides(0));
+
     }
 }
